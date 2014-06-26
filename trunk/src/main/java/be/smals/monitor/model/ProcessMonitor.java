daa01a33923e113f5d1e7a0bdab037a3f99a2b95
soldiertt@gmail.com <soldiertt@gmail.com@1eb76d03-98d6-c244-982e-9560e972b92e>
2014-06-26 20:59:30 +0000

    first commit

---
+++ trunk/src/main/java/be/smals/monitor/model/ProcessMonitor.java
@@ -0,0 +1,46 @@
+package be.smals.monitor.model;
+
+import java.io.Serializable;
+
+/**
+ * Object to transfer between client and server.
+ *
+ * Created by soldiertt on 23-06-14.
+ */
+public class ProcessMonitor implements Serializable {
+
+    private String hostname;
+
+    private boolean running;
+
+    private String name;
+
+    public ProcessMonitor(String hostname, String name) {
+        this.hostname = hostname;
+        this.name = name;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
